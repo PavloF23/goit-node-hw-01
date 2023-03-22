2b7775ba56596975d4bdfcb154d32fe2b9c9dfc1
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-03-22 09:26:59 +0200

    update

---
+++ README.md
@@ -0,0 +1,26 @@
+# goit-node-hw-01
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+
+https://monosnap.com/file/w3Em1SQhiJoBXgkQmB7cGJl0eTWwD8
+
+# Отримуємо контакт по id
+
+node index.js --action="get" --id=5
+
+https://monosnap.com/file/ldLfFuX6IG1xqi2gVsNMf6c9ytPqy1
+
+# Додаємо контакт
+
+node index.js --action="add" --name="Mango" --email="mango@gmail.com"
+--phone="322-22-22"
+
+https://monosnap.com/file/8wummyiAeaYst3wnr0mSANL0NAD0Mj
+
+# Видаляємо контакт
+
+node index.js --action="remove" --id=3
+
+https://monosnap.com/file/JdkM2M0sVmV0hYqzWp8CIZNhVgDZcV
--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 const { listContacts, getContactById, removeContact, addContact, } = require('./contacts');
 
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contact = await getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id ${id} does not exist`);
+      }
+      console.log(contact);
+      break;
+
+    case 'add':
+      const addedContact = await addContact(name, email, phone);
+      console.log(addedContact);
+      break;
+
+    case 'remove':
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
